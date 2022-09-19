b55baebb5be4ee2bd5611d5bd7e5ed1f6b81870c
Márcio Moura <marcioavb.ms@gmail.com>
2022-09-19 15:16:44 -0300

    implementa get cliente atraves do id

--- src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteApi.java
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteApi.java
@@ -1,16 +1,19 @@
 package br.com.petz.clientepet.cliente.application.api;
 
 import java.util.List;
+import java.util.UUID;
 
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/v1/cliente")
@@ -24,4 +27,12 @@ public interface ClienteApi {
 	@ResponseStatus(code = HttpStatus.OK)
 	List<ClienteListResponse> getTodosCliente();
 
+	@GetMapping(value = "/{idClinete}") 
+	@ResponseStatus(code = HttpStatus.OK)
+	ClienteDetalhadoResponse getClienteAtravesId(@PathVariable UUID idCliente);
+
 }
+
+
+
+
